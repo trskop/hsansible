94da8f4c31af891041f9c40b022a145c819e66b1
Peter Trško <peter.trsko@gmail.com>
2013-03-10 17:22:33 +0100

    Removed unnecessary white space

--- tools/sandbox-install.sh
+++ tools/sandbox-install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Copyright (c) 2013, Peter Trsko <peter.trsko@gmail.com>
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     * Redistributions in binary form must reproduce the above
 #       copyright notice, this list of conditions and the following
 #       disclaimer in the documentation and/or other materials provided
 #       with the distribution.
-# 
+#
 #     * Neither the name of Peter Trsko nor the names of other
 #       contributors may be used to endorse or promote products derived
 #       from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -42,19 +42,19 @@ Options:
 
 -C, --cabal-install
   Use cabal-install for installation.
-  
+
   Default if cabal-dev is not installed.
 
 -c, --cabal-dev
   Use cabal-dev for installation.
-  
+
   Default if cabal-dev is installed.
 
 -w WORKING_DIR, --working-dir=WORKING_DIR
   Change working directory to WORKING_DIR before calling cabal-install or
   cabal-dev. This option is useful if you use this script via Ansible to
   prepare the environment.
-  
+
   Default value is ".".
 
 -h, --help
