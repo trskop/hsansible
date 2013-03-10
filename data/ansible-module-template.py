fe76765a5d0c712f7d7312205842a2ece3e99118
Peter Trško <peter.trsko@gmail.com>
2013-03-10 17:32:21 +0100

    Renaming module template file
    It makes more sense to have ".py" extension then ".st".

--- data/ansible-module-template.py
+++ data/ansible-module-template.py
@@ -4,24 +4,24 @@
 # with following copyringht notice.
 #
 # Copyright (c) 2013, Peter Trsko <peter.trsko@gmail.com>
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     * Redistributions in binary form must reproduce the above
 #       copyright notice, this list of conditions and the following
 #       disclaimer in the documentation and/or other materials provided
 #       with the distribution.
-# 
+#
 #     * Neither the name of Peter Trsko nor the names of other
 #       contributors may be used to endorse or promote products derived
 #       from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -35,9 +35,8 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 $if(documentation)$
-DOCUMENTATION = \'\'\'
-$documentation$
-\'\'\'
+DOCUMENTATION = '''
+$documentation$'''
 $endif$
 
 import atexit
@@ -60,7 +59,7 @@ def remove_temporary_file(fn):
 def fail_json(msg):
     print json.dumps(dict(failed=True, msg=msg))
     sys.exit(1)
-    
+
 def main():
     try:
         fd, fn = tempfile.mkstemp()
@@ -81,9 +80,8 @@ def main():
     except Exception as e:
         fail_json("Error while calling executable: %s" % str(e))
 
-encodedBinary = \'\'\'
-$encodedBinary$
-\'\'\'
+encodedBinary = '''
+$encodedBinary$'''
 
 if __name__ == '__main__':
     main()
